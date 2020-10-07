026f207e68c932def78bdb46fbc3f09170f90d38
Kevyn Oliveira <kevyn_oliver@hotmail.com>
2020-10-06 20:13:41 -0400

    config: add eslintignore

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,6 +19,7 @@ module.exports = {
   },
   plugins: ['react', '@typescript-eslint', 'prettier'],
   rules: {
+    'no-console': 'off',
     'no-use-before-define': 'off',
     'prettier/prettier': 'error',
     'react/jsx-filename-extension': [
