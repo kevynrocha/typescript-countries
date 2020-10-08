e51ce15b2d78a2dd1dd900ef612f84b389dd282a
Kevyn Oliveira <kevyn_oliver@hotmail.com>
2020-10-07 23:45:42 -0400

    fix: add responsive select component

--- src/components/Select/styles.ts
+++ src/components/Select/styles.ts
@@ -4,7 +4,15 @@ export const CountrySelect = styled.select`
   background-color: ${({ theme }) => theme.colors.element};
   color: ${({ theme }) => theme.colors.text};
   border-radius: 4px;
-  max-width: 200px;
+  width: 50%;
   border: 0;
   padding: 0 20px;
+  display: flex;
+  height: 40px;
+  margin-top: 16px;
+
+  @media (min-width: 768px) {
+    margin-top: 0;
+    max-width: 200px;
+  }
 `;
