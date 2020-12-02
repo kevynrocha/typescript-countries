60e239e077fd11e46da7485be6a949df4f913cca
Kevyn Oliveira <kevyn_oliver@hotmail.com>
2020-12-01 20:39:53 -0400

    refactor(button): add styles and left icon

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,16 +1,28 @@
-import React from 'react';
-import { CountryButton } from './styles';
+import React, { useContext } from 'react';
+import { ThemeContext } from 'styled-components';
 
-interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+import * as S from './styles';
+
+interface ButtonProps {
   title: string;
+  leftIcon?: boolean;
+  to: string;
 }
 
-const Button: React.FC<ButtonProps> = ({ title, ...props }: ButtonProps) => {
+const Button: React.FC<ButtonProps> = ({
+  title,
+  leftIcon,
+  to,
+  ...props
+}: ButtonProps) => {
+  const themeContext = useContext(ThemeContext);
+
   console.log('button');
   return (
-    <CountryButton type="submit" {...props}>
-      {title}
-    </CountryButton>
+    <S.CountryButton to={to} {...props}>
+      {leftIcon && <S.BackIcon color={themeContext.colors.text} />}
+      <S.Title>{title}</S.Title>
+    </S.CountryButton>
   );
 };
 
--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,6 +1,29 @@
 import styled from 'styled-components';
+import { FiArrowLeft } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 
-export const CountryButton = styled.button`
-  background-color: 'black';
-  color: white;
+export const CountryButton = styled(Link)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  background-color: ${({ theme }) => theme.colors.element};
+  box-shadow: 0 0 4px ${({ theme }) => theme.colors.shadow};
+  color: ${({ theme }) => theme.colors.text};
+  text-decoration: none;
+  border-radius: 4px;
+  height: 40px;
+  width: 120px;
+`;
+
+export const BackIcon = styled(FiArrowLeft).attrs({
+  size: 20,
+})`
+  margin-right: 10px;
+`;
+
+export const Title = styled.h3`
+  color: ${({ theme }) => theme.colors.text};
+  font-size: 1rem;
+  font-weight: normal;
 `;
