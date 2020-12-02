68714e9c035a5dc56aff41c6b0065b4dea862ef3
Kevyn Oliveira <kevyn_oliver@hotmail.com>
2020-12-01 20:42:19 -0400

    refactor(select): change width/box-shadow/cursos styles

--- src/components/Select/styles.ts
+++ src/components/Select/styles.ts
@@ -3,13 +3,15 @@ import styled from 'styled-components';
 export const CountrySelect = styled.select`
   background-color: ${({ theme }) => theme.colors.element};
   color: ${({ theme }) => theme.colors.text};
+  box-shadow: 0 0 4px ${({ theme }) => theme.colors.shadow};
   border-radius: 4px;
-  width: 50%;
+  width: 100%;
   border: 0;
   padding: 0 20px;
   display: flex;
   height: 40px;
   margin-top: 16px;
+  cursor: pointer;
 
   @media (min-width: 768px) {
     margin-top: 0;
