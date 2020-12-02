158b8d2f5dac317020c24628bf02c68bb7ec9b64
Kevyn Oliveira <kevyn_oliver@hotmail.com>
2020-12-01 21:43:20 -0400

    refactor: change div to main

--- src/components/Layout/styles.ts
+++ src/components/Layout/styles.ts
@@ -4,7 +4,7 @@ export const Wrapper = styled.div`
   min-height: 100vh;
 `;
 
-export const Container = styled.div`
+export const Container = styled.main`
   margin: 32px auto;
   padding: 0 16px;
   max-width: 1150px;
